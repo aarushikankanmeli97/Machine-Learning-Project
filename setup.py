8d3232384cab04e4816fa81abdda52f11e48cc7a
aarushi <aarushi.kankanmeli@gmail.com>
2026-03-04 11:08:11 +0100

    project structure created

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements mentioned in the requirements.txt file
+    '''
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]  # Remove newline characters from each requirement
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+        return requirements
+    
+
+
+setup(
+    name='mlproj',
+    version='0.0.1',
+    author='Aarushi',
+    author_email='aarushi.kankanmeli@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
